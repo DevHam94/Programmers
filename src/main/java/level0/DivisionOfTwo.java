25ce23a56eef83ae4160c4bde0b45d0af31c3a5d
angryduck08 <gkaeogus7@gmail.com>
2022-12-12 20:19:59 +0900

    Feat: 22.12.12 level0

---
+++ src/main/java/level0/DivisionOfTwo.java
@@ -0,0 +1,8 @@
+package main.java.level0;
+
+public class DivisionOfTwo {
+    public int solution(int num1, int num2) {
+        int answer = (int)(((double)num1/num2)*1000);
+        return answer;
+    }
+}
---
+++ src/main/java/level0/SubstrationOfTwo.java
@@ -0,0 +1,8 @@
+package main.java.level0;
+
+public class SubstrationOfTwo {
+    public int solution(int num1, int num2) {
+        int answer = num1 - num2;
+        return answer;
+    }
+}
